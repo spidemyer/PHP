11eed7fa15c48fbeead42ed825db1eb85cd2cf4b
Emily <emilymima.jessica@gmail.com>
2026-04-23 16:55:35 -0300

    first commit

---
+++ introdução/Aula1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini Desafio</title>
+</head>
+<body>
+    <h1>Mensagens em PHP</h1>
+    <?php 
+        echo "<h2> Seja muito be-vindo a nossa empresa! </h2>";
+        echo "<p> Qual é o seu nome? </p>";
+        echo "<p><strong> Que legal!!!</strong></p>"
+        //site de interação sem interação
+     ?>
+</body>
+</html>
---
+++ introdução/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro PHP</title>
+</head>
+<body>
+    <h1>PHP abaixo:</h1>
+    <?php
+        echo "<h1> Isso é um título</h1>";
+        echo "<p> Isso é um paragrafo</p>";
+        echo "<strong> isso é negrito </strong>"
+        //comentario
+        #comentario tbm
+        /*olha so outro comentario*/
+    ?>
+
+</body>
+</html>
