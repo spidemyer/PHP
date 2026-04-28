a82d7edf762d7acb0b7daf5927cdcd5e0d676b66
Emily <emilymima.jessica@gmail.com>
2026-04-28 14:06:51 -0300

    boolean

--- Aula2/AT1.php
+++ Aula2/AT1.php
@@ -20,9 +20,11 @@
     $anoFundacao = 2009;
     echo "<p> Nossa empresa foi fundada em $anoFundacao.</p>";
     $quantidadeFuncionarios = 40;
-    echo "<p> Atualmente temos $quantidadeFuncionarios ativos na empresa.";    $empresaAtiva = true;
-    $empresaAtiva = true;
-    
+    echo "<p> Atualmente temos $quantidadeFuncionarios ativos na empresa.";    
+    $faturamento = 2000000.00;
+    echo "<p> Nosso Faturamento é de $faturamento";
+    $empresaAtiva = false;
+    echo "<p> A empresa está: " . ($empresaAtiva? "Ativa":"Desativada.");
     ?>
 </body>
 </html>
